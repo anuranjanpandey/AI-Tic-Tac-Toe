4e2761d66094ff0cb5cb440efd2b0b501c399ce9
Anuranjan <anuranjanpandey5@gmail.com>
2019-10-18 00:08:13 +0530

    Completed the game

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,9 @@
 #tictactoe
+"""
+Created by : Anuranjan Kumar pandey
+Date : 18/10/2019
 
+"""
 
 def displayBoard(board):
     for row in board:
@@ -10,9 +14,9 @@ def displayBoard(board):
 
 
 def updateBoard(r, c, sym, board):
-    if r < 0 and r >= len(board):
+    if r < 0 or r >= len(board):
         return False
-    elif c < 0 and c >= len(board[r]):
+    elif c < 0 or c>= len(board[r]):
         return False
     elif board[r][c] != '[ ]':
         return False
@@ -21,11 +25,77 @@ def updateBoard(r, c, sym, board):
     return True
 
 
+def isFull(board):
+    for row in board:
+        for col in row:
+            if col == '[ ]':
+                return False
+    return True
+
+
+def checkWins(board, sym):
+    sym = '[' + sym + ']'
+
+    # Row check
+    i = 0
+    while i < 3:
+        if board[i][0] == sym and board[i][1] == sym and board[i][2] == sym:
+            return True
+        i += 1
+
+    # Col check
+    i = 0
+    while i < 3:
+        if board[0][i] == sym and board[1][i] == sym and board[2][i] == sym:
+            return True
+        i += 1
+
+    # Diagonal check
+    if board[0][0] == sym and board[1][1] == sym and board[2][2] == sym:
+        return True
+
+    # Reverse diagonal check
+    if board[0][2] == sym and board[1][1] == sym and board[2][0] == sym:
+        return True
+
+    return False
+
+
 def tictactoe():
     board = [['[ ]']*3, ['[ ]']*3, ['[ ]']*3]
-    displayBoard(board)
-    updateBoard(1, 1, 'X', board)
-    displayBoard(board)
+    players = []
+    symbols = ['X','O']
+
+    players.append(input('Enter name of Player1 : '))
+    players.append(input('Enter name of Player2 : '))
+
+    i = 0
+    while i < 2:
+        print( 'Symbols of ' + players[i] + ' is ' + symbols[i])
+        i += 1
+
+    i = 0
+    flag = 1
+
+    while not isFull(board):
+        displayBoard(board)
+        print( players[i] + ' (' + symbols[i] + ') ' + 'plays :')
+        r = int(input("Enter row (0 - 2) :"))
+        c = int(input("Enter col (0 - 2) :"))
+
+        if updateBoard(r, c, symbols[i], board):
+            if checkWins(board, symbols[i]):
+                displayBoard(board)
+                print(players[i] + ' (' + symbols[i] + ') '+ 'Wins !!!')
+                flag = 0
+                break
+            i = (i +1)%2 # Switch Players
+        else:
+            print('Invalid Move.')
+
+    if flag == 1:
+        displayBoard(board)
+        print('Game Draw !!!')
 
 
 def main():
